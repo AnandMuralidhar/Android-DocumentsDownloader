1a26bfea694f551a2e8c395a2f3877ced3162560
Anand Muralidhar <anandmuralidhar111@gmail.com>
2019-03-29 19:51:39 -0700

    Updated MainActivity

--- app/src/main/java/com/anand/assignmentservices/MainActivity.java
+++ app/src/main/java/com/anand/assignmentservices/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,29 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void DownloadFiles(View view)
     {
-        CheckStoragePermission();
         Intent intent = new Intent(this, DownloadActivity.class);
         startActivity(intent);
-    }
-
-    public boolean CheckStoragePermission() {
-        if (Build.VERSION.SDK_INT >= 23)
-        {
-                if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-                {
-                    Log.e("Error", "You have permission");
-                    return true;
-                }
-                else {
-                Log.e("Error", "You have asked for permission");
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                return false;
-                }
-        }
-        else {
-            Log.e("Error", "You already have the permission");
-            return true;
-        }
     }
 
     public void CloseApplication(View view)
